cb64237cbf7614f70df8f4c5c348ee21964f7201
GulshanSharma0 <gulkumar8138108@gmail.com>
2022-12-01 19:43:08 +0530

    first commit

---
+++ HuffmanClass.py
@@ -0,0 +1,17 @@
+class HuffManCoding:
+    def __init__(self,path):
+        self.path=path
+    def _frequency_from_text(self,text):
+
+    def compression(self):
+
+        
+        #To access the file and extract text from that file.
+        #Calculate frequency of each text and store it in freq.dictionary
+        #Min heap for two minimum frequency.
+        #Construct binary tree from heap.
+        #Construct code from binary tree and stored it in dictionary.
+        #Construct encoded text.
+        #we have return that file as an output.
+
+
